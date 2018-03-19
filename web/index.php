7984476c3f05522945ee33a9457237c4c5a8e9e6
EDEN45 <futurionerv@outlook.com>
2018-03-20 00:17:13 +0700

    vk_code

--- web/index.php
+++ web/index.php
@@ -15,10 +15,31 @@ $app->get('/', function() use($app) {
   return "bot-math-1155";
 });
 
+// Our web handlers
 $app->post('/bot', function() use($app) {
 
+	$data = json_decode(file_get_contents('php://input'));
 
-  return "bot-math-1155";
+	if (!$data)
+	{
+		return "bot-math-1155";
+	}
+
+	if ($data->secret !== getenv('VK_TOKEN') && $data->type !== 'confirmation')
+	{
+		return "bot-math-1155";
+	}
+
+	switch ($data->type) {
+		case 'confirmation':
+			return getenv('VK_RETURN_KEY');
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+  return "bot-math-1155" ;
 });
 
 $app->run();
