ef49943014a712604e0caee9984b83c93e177a39
EDEN45 <futurionerv@outlook.com>
2018-03-26 22:01:57 +0700

    vk

--- web/index.php
+++ web/index.php
@@ -62,7 +62,7 @@ $app->post('/bot', function() use($app) {
 			$text = mb_strtolower($data->object->body);
 			$text_s = 0;
 			foreach ($phrases as $key => $value) {
-				if ($value == $text)
+				if ($key == $text)
 				{
 					$text_s = 1;
 				}
